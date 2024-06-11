707faef94898e68f1fc1942c68178c85dfdbcbe0
mateuszw-tech <mateuszw.tech@gmail.com>
2024-06-11 23:50:48 +0200

    Add goal function

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def goal_function(subset: list, target: int) -> int:
+    return abs(sum(subset) - target)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hssissss, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
